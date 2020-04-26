a1c9d29ee7b68bbed2ba3813b29f2691b612df6d
이성현 <>
2020-04-26 19:05:27 +0900

    설정 파일 편집

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,34 @@
-module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+{
+  "parser": "babel-eslint",
+  "extends": "airbnb",
+  "env": {
+    "browser": true
+  },
+  "plugins": [
+    "babel"
+  ],
+  "rules": {
+    "react/prop-types": 0,
+    "react/jsx-wrap-multilines": 0,
+    "react/state-in-constructor": 0,
+    "react/forbid-prop-types": 0,
+    "max-len": ["error", 150, 2, {
+      "ignoreUrls": true,
+      "ignoreTrailingComments": true
+    }],
+    "object-curly-newline": 0,
+    "react/jsx-filename-extension": [1, {
+      "extensions": [".js", ".jsx"]
+    }]
+  },
+  "settings": {
+    "import/resolver": {
+      "node": {
+        "moduleDirectory": [
+          "node_modules",
+          "."
+        ]
+      }
+    }
+  }
+}
